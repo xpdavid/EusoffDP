fb765daca2ac4bd209fe1db00c00934074e0c8c6
fanshicomic <fanshicomic@gmail.com>
2014-12-13 01:06:28 +0800

    update redirect page info and style

--- booking.php
+++ booking.php
@@ -21,48 +21,7 @@
 	}
 
 	if ($success){
-		echo <<<REDIRECT
-<!doctype html>
-<html>
-<head>
-	<style type="text/css">
-	#msgbox {
-		position: absolute;
-		margin-left: auto;
-		margin-right: auto;
-		left: 0;
-		right: 0;
-		top:25%;
-		height: 150px;
-		width: 400px;
-		border: 10px solid grey;
-		border-radius: 2%;
-	}
-
-	#msg {
-		margin-top:13%;
-		color:grey;
-		text-align:center;
-		font-size:25px;
-		font-family: "Times New Roman";
-	}
-	</style>
-</head>
-
-<body>
-	<!--<div id='msgbox'>
-		<p id='msg'>Booking success!
-		<br>Redirecting to homepage...</p>
-	</div>-->
-	<div style="text-align:center; margin-top:20%">Congratulations! Your booking information is successfully recorded, please contact XXX for payment and ticket collection.</div>
-	<script type='text/javascript'>
-		setTimeout(function() {window.location.href='index.html'},3000);
-	</script>
-</body>
-
-
-</html>
-REDIRECT;
+		header("Location: /dp1415/booking_redirect.html");
 	} else {
 
 	}
