d4b565a98a82e95ef50aa80a53e96963361a5139
elijahsk <song.kai@dhs.sg>
2015-12-09 09:36:28 +0800

    Updated track.php and db.php

--- track.php
+++ track.php
@@ -3,15 +3,15 @@
 		$con = establish();
 		mysql_select_db(DB_NAME, $con);
 
-		$query1 = "SELECT book_id, exp_time FROM " . BOOKING_TABLE . " WHERE status = '" . BOOKING_STATUS_PENDING . "'";
-		$result1 = mysql_query($query1);
+		$query = "SELECT book_id, exp_time FROM " . BOOKING_TABLE . " WHERE status = '" . BOOKING_STATUS_PENDING . "'";
+		$result = mysql_query($query1);
 
 		if ($result){
-			$cur_time   =   strtotime(now);
+			$cur_time  =  strtotime(now);
 			while ($row = $result->fetch_assoc()){
-				$booking_exp_time = strtotime(row["exp_time"]);
+				$booking_exp_time = strtotime($row["exp_time"]);
 				if ($booking_exp_time < $cur_time){
-					cancel_booking(row["book_id"]);
+					cancel_booking($row["book_id"]);
 				}
 
 			}
