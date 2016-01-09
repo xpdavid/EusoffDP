e4a2ab259ce70225bdc1800cf5efbbcaba3c8c1d
elijahsk <song.kai@dhs.sg>
2016-01-09 23:57:02 +0800

    Add .50 charge for registered mail;Modified contact page, home page and synopsis page

--- home.php
+++ home.php
@@ -15,15 +15,18 @@
   <!--<![endif]-->
   <link rel="stylesheet" href="static/css/layouts/marketing.css" />
 	<style type="text/css">
+		body {
+			overflow: hidden;
+		}
 		#info {
-			background-color: rgba(0, 0, 0, 0.5);
-			margin-right: 20px;
-			font-family: "helvetica";
+			background-color: rgba(0, 0, 0, 0);
+			margin-right: 0px;
+			font-family: Verdana;
 			font-size: 15px;
 			text-align: left;
-			padding: 15px;
+			padding: 1px;
 			border-radius: 5px;
-			color: white;
+			color: #444e53;
 		}
 	</style>
 </head>
@@ -31,24 +34,21 @@
   <div class="container_p" style="margin-left: 29%">
     <a href="index.php"><img src="img/title.png" width="700" height="200"></a>
   </div>
-	<div class="container_p" style="backgroud: rga(0,0,0,0.5)">
+	<div class="container_p" style="backgroud: rga(0,0,0,0.5); width:90%;">
 		<div class="pure-g">
-			<div class="pure-u-1-2">
-				<img src="img/Poster_final.jpg" width="280" height="400">
-			</div>
-			<div class="pure-u-1-2">
+			<div class="pure-u-5-12"></div>
+			<div class="pure-u-7-12">
 		
 			<div id="info">
-				<p>
-					Since its inaugural show in 1991, Eusoff Hall Dance Production has been a consistent student-initiated effort managed entirely by the residents of Eusoff hall to pursue their passion for the arts. Besides collaborating to stage a spectacular fusion of dance and drama, we are also involved in the backstage planning of the whole production. All in all, almost 200 residents are involved. Taking months of preparation and planning, show day itself is a culmination of five months of sheer hard work. And each year, countless friendships are forged as the production team strives together in a pursuit for excellence.</p>
+				<p>	Since its inaugural show in 1991, Eusoff Hall Dance Production has been a consistent student-initiated effort managed entirely by the residents of Eusoff hall to pursue their passion for the arts. Besides collaborating to stage a spectacular fusion of dance and drama, we are also involved in the backstage planning of the whole production. All in all, almost 200 residents are involved. Taking months of preparation and planning, show day itself is a culmination of five months of sheer hard work. And each year, countless friendships are forged as the production team strives together in a pursuit for excellence.</p>
 
-<p>Members tap on their creativity to give life to the story as they work to ensure that sets, props and wardrobe complement the script. Through practices and trainings, the production team develops together as they build up their repertoire of skills relevant to staging a show. This ranges from the tailoring of costumes to the construction of sets to performing to designing light and sound effects. Thus, members have to create synergy amongst themselves to coordinate with each other seamlessly in order to make the production a success.</p>
+				<p>Members tap on their creativity to give life to the story as they work to ensure that sets, props and wardrobe complement the script. Through practices and trainings, the production team develops together as they build up their repertoire of skills relevant to staging a show. This ranges from the tailoring of costumes to the construction of sets to performing to designing light and sound effects. Thus, members have to create synergy amongst themselves to coordinate with each other seamlessly in order to make the production a success.</p>
 
-<p>With that, our dreams for Eusoff Hall Dance Production are big as we hope that it would serve as a platform for Eusoff residents to pursue their passion for the arts and culture. With the time invested in rigorous trainings and practices, we hope to stretch our team in their development of skills.  And so, we look forward to you on show night to enjoy the fruits of our labour!</p>
+				<p>With that, our dreams for Eusoff Hall Dance Production are big as we hope that it would serve as a platform for Eusoff residents to pursue their passion for the arts and culture. With the time invested in rigorous trainings and practices, we hope to stretch our team in their development of skills.  And so, we look forward to you on show night to enjoy the fruits of our labour!</p>
 
-<p>With love,</p>
+				<p>With love,</p>
 
-<p>Eusoff Hall Dance Production Team</p>
+				<p>Eusoff Hall Dance Production Team</p>
 			</div>
 		</div>
 		</div>
--- synopsis.php
+++ synopsis.php
@@ -16,34 +16,32 @@
   <link rel="stylesheet" href="static/css/layouts/marketing.css" />
   <style type="text/css">
 		.info {
-			background-color: rgba(0, 0, 0, 0.5);
 			margin-right: 20px;
 			font-family: "helvetica";
 			font-size: 15px;
 			text-align: left;
-			padding: 15px;
 			border-radius: 5px;
-			color: white;
+			color: #444e53;
 		}
 	</style>
 </head>
 <body>
-  <div class="container_p" style="margin-left: 29%">
+  <div class="container_p" style="margin-left:30%">
     <a href="index.php"><img src="img/title.png" width="700" height="200"></a>
   </div>
-	<div class="container_p" style="backgroud: rga(0,0,0,0.5)">
+	<div class="container_p" style="width:95%;">
 		<div class="pure-g">
-			<div class="pure-u-1-2">
+			<div class="pure-u-5-12">
 			</div>
-			<div class="pure-u-1-2">
-				<iframe src="http://www.youtube.com/embed/tNLVoPN1chg" style="width:480px;height:285px">
+			<div class="pure-u-7-12">
+				<iframe src="http://www.youtube.com/embed/tNLVoPN1chg" style="width:395px;height:230px">
 				</iframe>
 			</div>
 		</div>
 		<div class="pure-g">
-			<div class="pure-u-1-2">
+			<div class="pure-u-5-12">
 			</div>
-			<div class="pure-u-1-2">
+			<div class="pure-u-7-12">
 				<div class="info">
 				<p>This is a story. Like most other stories, this is a story about people. What happens when you take a pair of orphaned siblings and give one of them the ability to bring life to a complete standstill? Aaron and Brenda have never known their parents and like any other orphans, they have none to rely on but each other. However, when Aaron’s greed gets the better of him, he takes away his sister’s chance at a scholarship, disappearing from her life for seven years. Guilt-stricken, his return to Singapore is marked by his meteoric rise through the ranks in the corporate world and his clumsy steps towards redemption. What happens when he eventually confesses his remorse? People are rarely ever completely good or bad. Join us as we celebrate kinship, love and self-abnegation.</p>
 
