03addfbf73b6d154c85aa7b627d473ccd682eab0
Xiao Pu <acesscp@gmail.com>
2016-01-12 22:31:20 +0800

    send email after booking comfirmed

--- admin/include/get_booking.php
+++ admin/include/get_booking.php
@@ -2,7 +2,7 @@
 	require_once("../../include/constant.php");
 	require_once("db.php");
 	session_start();
-	if (isset($_SESSION["login"]) || $_SESSION["status"]) {
+	if (isset($_SESSION["login"]) || $_SESSION["login"]) {
 		echo get_booking($_REQUEST['status']);
 	} else {
 		echo "";
